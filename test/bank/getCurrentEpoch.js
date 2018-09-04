d6911a176b779d7a6532a31820e9c69b3489fb64
Isaac Kang <isaac.kang@consensys.net>
2018-09-04 14:52:12 -0400

    use epochDuration value to increaseTime

--- test/bank/getCurrentEpoch.js
+++ test/bank/getCurrentEpoch.js
@@ -1,7 +1,5 @@
 /* eslint-env mocha */
-/* global assert contract artifacts */
-const Bank = artifacts.require('Bank.sol');
-
+/* global assert contract */
 const utils = require('../utils.js');
 
 contract('Bank', (accounts) => {
@@ -9,30 +7,34 @@ contract('Bank', (accounts) => {
     let token;
     let voting;
     let registry;
+    let bank;
+    let epochDuration;
 
     beforeEach(async () => {
-      const { votingProxy, registryProxy, tokenInstance } = await utils.getProxies();
+      const {
+        votingProxy, registryProxy, tokenInstance, bankInstance,
+      } = await utils.getProxies();
       voting = votingProxy;
       registry = registryProxy;
       token = tokenInstance;
+      bank = bankInstance;
+      epochDuration = (await bank.EPOCH_DURATION.call()).toNumber();
 
       await utils.approveProxies(accounts, token, voting, null, registry);
     });
 
     it('should return the correct epoch each time it is invoked', async () => {
-      const bankAddress = await registry.bank.call();
-      const bank = Bank.at(bankAddress);
-
       const e1 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e1.toString(), '0', 'initial epoch should be 0');
 
-      await utils.increaseTime(180);
+      await utils.increaseTime(epochDuration);
       const e2 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e2.toString(), '1', 'second epoch should be 1');
 
       const e3 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e3.toString(), '1', 'epoch should still be 1 since no time has passed');
 
+      // incrementally increase the time NOT beyond the epochDuration
       await utils.increaseTime(10);
       const e4 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e4.toString(), '1', 'epoch should still be 1 since not enough time has passed');
@@ -41,7 +43,8 @@ contract('Bank', (accounts) => {
       const e5 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e5.toString(), '1', 'epoch should still be 1 since not enough time has passed');
 
-      await utils.increaseTime(160);
+      // increase the time beyond the epochDuration
+      await utils.increaseTime(epochDuration - 20);
       const e6 = await bank.getCurrentEpoch.call();
       assert.strictEqual(e6.toString(), '2', 'epoch should be 2 since enough time has passed');
     });
