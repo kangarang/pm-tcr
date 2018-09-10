0d061d1d8b2910cdd9565070dd96312bf2603eb7
Isaac Kang <isaac.kang@consensys.net>
2018-09-10 12:40:55 -0400

    add proxy deployment script

--- scripts/deploy_proxies.js
+++ scripts/deploy_proxies.js
@@ -1,22 +1,32 @@
-/* global artifacts network */
+/* global artifacts web3 */
 const fs = require('fs');
+const BN = require('bignumber.js');
 
 const RegistryFactory = artifacts.require('RegistryFactory.sol');
+const Registry = artifacts.require('Registry.sol');
+const Token = artifacts.require('EIP20.sol');
 
 const config = JSON.parse(fs.readFileSync('../conf/config.json'));
 const paramConfig = config.paramDefaults;
 
 module.exports = (done) => {
   async function deployProxies(networkID) {
-    const registryFactoryAddress = (
-      networkID === '1' ? '0xcc0df91b86795f21c3d43dbeb3ede0dfcf8dccaf' // mainnet
-      : networkID === '4' ? '0x822415a1e4d0d7f99425d794a817d9b823bdcd0c' // rinkeby
-      : RegistryFactory.address // development
-    );
+    let registryFactoryAddress;
+    if (networkID === '1') {
+      registryFactoryAddress = '0xcc0df91b86795f21c3d43dbeb3ede0dfcf8dccaf'; // mainnet
+    } else if (networkID === '4') {
+      registryFactoryAddress = '0x822415a1e4d0d7f99425d794a817d9b823bdcd0c'; // rinkeby
+    } else {
+      registryFactoryAddress = RegistryFactory.address; // development
+    }
 
-    console.log(`RegistryFactory:   ${registryFactoryAddress}`);
+    /* eslint-disable no-console */
+    console.log('Using RegistryFactory at:');
+    console.log(`     ${registryFactoryAddress}`);
     console.log('');
     console.log('Deploying proxy contracts...');
+    console.log('...');
+    /* eslint-enable no-console */
 
     const registryFactory = await RegistryFactory.at(registryFactoryAddress);
     const registryReceipt = await registryFactory.newRegistryWithToken(
@@ -48,14 +58,33 @@ module.exports = (done) => {
       registry,
     } = registryReceipt.logs[0].args;
 
+    const registryProxy = await Registry.at(registry);
+    const tokenProxy = await Token.at(token);
+    const registryName = await registryProxy.name.call();
+
+    /* eslint-disable no-console */
+    console.log(`Proxy contracts successfully migrated to network_id: ${networkID}`);
     console.log('');
-    console.log(`Proxy contracts successfully migrated to network_id: ${networkID}`)
+    console.log(`${config.token.name} (EIP20):`);
+    console.log(`     ${token}`);
+    console.log('PLCRVoting:');
+    console.log(`     ${plcr}`);
+    console.log('Parameterizer:');
+    console.log(`     ${parameterizer}`);
+    console.log(`${registryName} (Registry):`);
+    console.log(`     ${registry}`);
     console.log('');
-    console.log(`${config.token.name}:          ${token}`);
-    console.log(`PLCRVoting:        ${plcr}`);
-    console.log(`Parameterizer:     ${parameterizer}`);
-    console.log(`Registry:          ${registry}`);
+
+    const evenTokenDispensation =
+      new BN(config.token.supply).div(config.token.tokenHolders.length).toString();
+    console.log(`Dispensing ${config.token.supply} tokens evenly to ${config.token.tokenHolders.length} addresses:`);
     console.log('');
+
+    await Promise.all(config.token.tokenHolders.map(async (account) => {
+      console.log(`Transferring tokens to address: ${account}`);
+      return tokenProxy.transfer(account, evenTokenDispensation);
+    }));
+    /* eslint-enable no-console */
 
     return true;
   }
@@ -67,4 +96,4 @@ module.exports = (done) => {
     }
     return deployProxies(network).then(() => done());
   });
-}
+};
